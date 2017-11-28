adc19e53ea57fab3a7223cc67993527899b3d642
Vincent van der Palen <vincent@MacBook-Pro-van-Vincent.local>
2017-11-28 12:46:31 +0100

    adds project
    Built with HTML5 Boilerplate, fullPage.js and CSS Grid Layout

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$(document).ready(function () {
+    $('#fullpage').fullpage({
+        
+        //Navigation
+        anchors: ['Intro', 'Sans-Serif', 'Helvetica'],
+        menu: '#menu',
+        slidesNavigation: true,
+
+        //Design
+        sectionsColor: ['#1A1A1A', '#1A1A1A;', 'white', '#1A1A1A;', '#1A1A1A;'],
+        verticalCentered: false,
+        fadingEffect: 'slides',
+        fadingEffectKey: 'c25lYWt5dmluY2UubmxfNHh2Wm1Ga2FXNW5SV1ptWldOMHBFRw==',
+    });
+});
